bef853ceb1c928a353eae865db1b4e1bdc26615a
hiisuuii <dogglez7777@gmail.com>
2019-08-14 00:01:49 -0600

    Updated source to match v1.1.1

--- src/main/java/sparks/mod/potioncrackers/init/ModItems.java
+++ src/main/java/sparks/mod/potioncrackers/init/ModItems.java
@@ -17,19 +17,19 @@ public class ModItems
 	//items
 	
 	//food
-	public static final Item HEALING_CRACKER = new FoodEffectBase("healing_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.INSTANT_HEALTH, 1, 1, false, true));
-	public static final Item REGEN_CRACKER = new FoodEffectBase("regen_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.REGENERATION, 240, 1, false, true));
-	public static final Item SPEED_CRACKER = new FoodEffectBase("speed_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.SPEED, 2400, 0, false, true));;
-	public static final Item STRENGTH_CRACKER = new FoodEffectBase("strength_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.STRENGTH, 2400, 0, false, true));
-	public static final Item RESISTANCE_CRACKER = new FoodEffectBase("resistance_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.RESISTANCE, 2400, 1, false, true));
-	public static final Item HASTE_CRACKER = new FoodEffectBase("haste_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.HASTE, 2400, 1, false, true));
-	public static final Item FIRERESISTANCE_CRACKER = new FoodEffectBase("fireresistance_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.FIRE_RESISTANCE, 2400, 0, false, true));
-	public static final Item NIGHTVISION_CRACKER = new FoodEffectBase("nightvision_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.NIGHT_VISION, 2400, 0, false, true));
-	public static final Item BREATH_CRACKER = new FoodEffectBase("breath_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.WATER_BREATHING, 2400, 0, false, true));
-	public static final Item INVIS_CRACKER = new FoodEffectBase("invis_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.INVISIBILITY, 2400, 0, false, true));
-	public static final Item JUMP_CRACKER = new FoodEffectBase("jump_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.JUMP_BOOST, 2400, 0, false, true));
-	public static final Item ABSORB_CRACKER = new FoodEffectBase("absorb_cracker", 4, 9.6F, false, new PotionEffect(MobEffects.ABSORPTION, 2400, 0, false, true));
-	public static final Item BASE_CRACKER = new FoodBase("base_cracker", 2, 2.1F, false);
-	public static final Item CHEESE = new FoodBase("cheese", 7, 5.0F, false);
-	public static final Item ENCHANTED_CHEESE = new FoodMultiEffect("enchanted_cheese", 10, 10.0F, false);
+	public static final Item HEALING_CRACKER = new FoodEffectBase("healing_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.INSTANT_HEALTH, 1, 1, false, true));
+	public static final Item REGEN_CRACKER = new FoodEffectBase("regen_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.REGENERATION, 240, 1, false, true));
+	public static final Item SPEED_CRACKER = new FoodEffectBase("speed_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.SPEED, 2400, 0, false, true));;
+	public static final Item STRENGTH_CRACKER = new FoodEffectBase("strength_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.STRENGTH, 2400, 0, false, true));
+	public static final Item RESISTANCE_CRACKER = new FoodEffectBase("resistance_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.RESISTANCE, 2400, 1, false, true));
+	public static final Item HASTE_CRACKER = new FoodEffectBase("haste_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.HASTE, 2400, 1, false, true));
+	public static final Item FIRERESISTANCE_CRACKER = new FoodEffectBase("fireresistance_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.FIRE_RESISTANCE, 2400, 0, false, true));
+	public static final Item NIGHTVISION_CRACKER = new FoodEffectBase("nightvision_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.NIGHT_VISION, 2400, 0, false, true));
+	public static final Item BREATH_CRACKER = new FoodEffectBase("breath_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.WATER_BREATHING, 2400, 0, false, true));
+	public static final Item INVIS_CRACKER = new FoodEffectBase("invis_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.INVISIBILITY, 2400, 0, false, true));
+	public static final Item JUMP_CRACKER = new FoodEffectBase("jump_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.JUMP_BOOST, 2400, 0, false, true));
+	public static final Item ABSORB_CRACKER = new FoodEffectBase("absorb_cracker", 4, 1.5F, false, new PotionEffect(MobEffects.ABSORPTION, 2400, 0, false, true));
+	public static final Item BASE_CRACKER = new FoodBase("base_cracker", 2, 2.0F, false);
+	public static final Item CHEESE = new FoodBase("cheese", 6, 0.6F, false);
+	public static final Item ENCHANTED_CHEESE = new FoodMultiEffect("enchanted_cheese", 8, 1.25F, false);
 }
